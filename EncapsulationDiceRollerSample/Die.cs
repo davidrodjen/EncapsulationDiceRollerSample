361eb2e73c02a1a2d428cfc52f4e1cad2872f551
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-13 14:53:23 -0800

    Add project files.

---
+++ EncapsulationDiceRollerSample/Die.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDiceRollerSample
+{
+    /// <summary>
+    /// Represents a single 6-sided die
+    /// </summary>
+    class Die
+    {
+        /// <summary>
+        /// Static members get shared across all
+        /// instances of this class
+        /// </summary>
+        private static Random rand;
+
+        /// <summary>
+        /// runs once before any Die objects are
+        /// created and initializes random object once
+        /// </summary>
+        static Die()
+        {
+            rand = new Random();
+        }
+
+
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
+        /// The current value of the die
+        /// </summary>
+        public byte FaceValue { get; private set; } //private overrides the public
+
+        /*
+        public byte FaceValue
+        {
+            get { return FaceValue; }
+            private set
+            {
+                if (value < 1 || value > 6)
+                {
+                    throw new ArgumentException
+                }
+            }
+        }
+        */
+
+
+
+        /// <summary>
+        /// Saving the roll
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Generates a new random face value. sets face value,
+        /// returns the generated the face value
+        /// </summary>
+        public byte Roll()
+        {
+
+            // If current die is held, return the current value
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+
+            const byte MinValue = 1;
+            const byte MaxValue = 6;
+            // Offset is for the exclusive range of Random.Next();
+            const byte Offset = 1;
+
+            
+            byte newValue = (byte)rand.Next(MinValue, MaxValue + Offset);
+
+            FaceValue = newValue;
+            return newValue;
+        }
+    }
+}
---
+++ EncapsulationDiceRollerSample/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDiceRollerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Die[] dice = new Die[5];
+
+            InitDice(dice);
+            PrintDieFaceValues(dice);
+
+            // Hold first 3 dice
+            dice[0].IsHeld = true;
+            dice[1].IsHeld = true;
+            dice[2].IsHeld = true;
+
+            Console.WriteLine("\n\nHolding first 3, rerolling. \n\n");
+            RollAllDice(dice);
+            PrintDieFaceValues(dice);
+
+            /*
+            Die myDice = new Die();
+            // set is private and this is not allowed
+            // myDice.FaceValue = 6;
+            Console.WriteLine(myDice.FaceValue);
+            
+
+            myDice.Roll();
+
+            Console.WriteLine(myDice.FaceValue);
+
+            */
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Rolls all dice
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void RollAllDice(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i].Roll();
+            }
+        }
+
+
+        /// <summary>
+        /// Prints dice FaceValues
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void PrintDieFaceValues(Die[] dice)
+        {
+            // Print out values
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine(dice[i].FaceValue);
+            }
+        }
+
+        /// <summary>
+        /// Initializes all die in a given array
+        /// Assumes array has been given a length
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void InitDice(Die[] dice)
+        {
+            // Create 5 dice
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+        }
+    }
+}
